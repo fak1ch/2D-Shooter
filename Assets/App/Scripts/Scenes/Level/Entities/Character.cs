e826b717b7e2b1cf95b4d5b02eeaa5181f1c5645
fak1ch <slyvayva@mail.ru>
2023-05-23 21:44:34 +0300

    AS_develop: add reload scene

--- Assets/App/Scripts/Scenes/Level/Entities/Character.cs
+++ Assets/App/Scripts/Scenes/Level/Entities/Character.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using App.Scripts.General.LoadScene;
+using UnityEngine;
 
 namespace App.Scripts.Scenes.MainScene.Entities
 {
@@ -7,5 +10,22 @@ namespace App.Scripts.Scenes.MainScene.Entities
         public HealthComponent HealthComponent => _healthComponent;
         
         [SerializeField] private HealthComponent _healthComponent;
+
+        private void Start()
+        {
+            _healthComponent.OnHealthEqualsZero += CharacterDieCallback;
+        }
+
+        private void CharacterDieCallback()
+        {
+            gameObject.SetActive(false);
+            StartCoroutine(RestartSceneRoutine());
+        }
+
+        private IEnumerator RestartSceneRoutine()
+        {
+            yield return new WaitForSeconds(2f);
+            SceneLoader.Instance.LoadScene(SceneEnum.Level);
+        }
     }
 }
